1f5dff2b7545b2979daf6a9b67e205f409a980e9
paulhideg <paulhideg002@gmail.com>
2024-02-29 18:58:44 +0200

    Published to Github Pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 class App extends Component {
   constructor() {
@@ -35,7 +36,9 @@ class App extends Component {
         <h1>Robofriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
